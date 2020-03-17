bf8dc60c5ce3b7b5086321645f8b3479786e957b
jungu <gj99@mail.ustc.edu.cn>
2020-03-17 23:14:43 +0800

    put auto_link

---
+++ auto_link.sh
@@ -0,0 +1,9 @@
+############################################################
+# File Name: auto_link.sh
+# Author: JunGu
+# Mail: gj99@mail.ustc.edu.cn
+# Created Time: 2020年03月17日 星期二 22时53分45秒
+####################################################
+#!/bin/bash
+ln -fs $HOME/my_config/vim $HOME/.vim
+ln -fs $HOME/my_config/vimrc $HOME/.vimrc
